900030f4ab982d0f623983816d3f97e29f9b7c7a
milkakeza <milkakeza9@gmail.com>
2026-07-10 00:47:32 -0400

    Move MongoDB credentials to environment variables

--- api/config.py
+++ api/config.py
@@ -1,11 +1,13 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # -----------------------------
 # MySQL Configuration
 # -----------------------------
 DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "mysql+pymysql://root@localhost/walmart_sales_db"
+    "DATABASE_URL"
 )
 
 # -----------------------------
@@ -13,7 +15,6 @@ DATABASE_URL = os.getenv(
 # -----------------------------
 MONGODB_URI = os.getenv(
     "MONGODB_URI",
-    "mongodb+srv://nkusiyvette1_db_user:QGbZ8SmNc0u0WRSW@cluster0.nsxkun2.mongodb.net/?retryWrites=true&w=majority"
 )
 
 MONGODB_DB = "walmart_sales_db"
